82fa9d1d8468114668656268b3cd83af7d64472a
armiiinsaber <armin.sm2@gmail.com>
2025-08-31 19:17:13 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
+// next.config.js
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-module.exports = nextConfig;
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
+};
